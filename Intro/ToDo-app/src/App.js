316071babfd2bfa0d2fb3be6fb1cb2aba0a93fd9
skumarscse-skillsafari <sathish.kumar@skillsafari.in>
2022-10-19 20:41:35 +0530

    react-form-2

--- Intro/ToDo-app/src/App.js
+++ Intro/ToDo-app/src/App.js
@@ -1,21 +1,23 @@
 // import "./App.css";
 import { useState } from "react";
 import AddToDo from "./Components/AddToDo";
+import Form from "./Components/Form/Form";
 // import Demo from "./Components/Demo";
 import ToDoList from "./Components/ToDoList";
 
 function App() {
-  const [todos, setToDos] = useState([
-    { id: 1, text: "Book Ticket", done: false },
-    { id: 2, text: "Wash cloths", done: false },
-    { id: 3, text: "Write Homework", done: false },
-  ]);
-  console.log(todos);
+  // const [todos, setToDos] = useState([
+  //   { id: 1, text: "Book Ticket", done: false },
+  //   { id: 2, text: "Wash cloths", done: false },
+  //   { id: 3, text: "Write Homework", done: false },
+  // ]);
+  // console.log(todos);
   return (
     <div className="App">
-      <h1>To-Do List</h1>
+      <Form />
+      {/* <h1>To-Do List</h1>
       <ToDoList todos={todos} setToDos={setToDos} />
-      <AddToDo setToDos={setToDos} />
+      <AddToDo setToDos={setToDos} /> */}
       {/* <Demo /> */}
     </div>
   );
---
+++ Intro/ToDo-app/src/Components/Form/Form.js
@@ -0,0 +1,132 @@
+import { useState } from "react";
+function Form() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    address: "",
+    mode: "",
+    addOns: [],
+    department: "",
+  });
+  console.log(formData);
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  function handleChange(e) {
+    const { type, name, value } = e.target;
+    console.log(value);
+    type === "checkbox"
+      ? setFormData((prevData) => {
+          return {
+            ...prevData,
+            addOns: prevData.addOns.concat(value),
+          };
+        })
+      : setFormData((prevData) => {
+          return {
+            ...prevData,
+            [name]: value,
+          };
+        });
+    console.log(formData);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Enter the following details:</h2>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
+      </div>
+      <br />
+      <div>
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
+        />
+      </div>
+      <br />
+      <div>
+        <label htmlFor="address">Address:</label>
+        <textarea
+          name="address"
+          onChange={handleChange}
+          value={formData.address}
+        />
+      </div>
+      <br />
+      <div>
+        <label htmlFor="mode-of-study">Mode of study:</label>
+        <input
+          type="radio"
+          name="mode"
+          onChange={handleChange}
+          value="Part Time"
+        />
+        <label>Part Time</label>
+        <input
+          type="radio"
+          name="mode"
+          onChange={handleChange}
+          value="Full Time"
+        />
+        <label>Full Time</label>
+      </div>
+      <br />
+      <div>
+        <label htmlFor="add-ons">Add-Ons:</label>
+        <input
+          type="checkbox"
+          name="add-ons-1"
+          onChange={handleChange}
+          value="Hostel"
+        />
+        <label>Hostel</label>
+        <input
+          type="checkbox"
+          name="add-ons-2"
+          onChange={handleChange}
+          value="Bus"
+        />
+        <label>Bus</label>
+      </div>
+      <br />
+      <div>
+        <label htmlFor="department">Department:</label>
+        <select
+          name="department"
+          onChange={handleChange}
+          value={formData.department}
+        >
+          <option value="English">English</option>
+          <option value="Maths">Maths</option>
+          <option value="Physics">Physics</option>
+          <option value="Computer Science">Computer Science</option>
+        </select>
+      </div>
+      <br />
+      <div>
+        <button>Submit</button>
+      </div>
+      <div>
+        <h1>{formData.name}</h1>
+        <p>{formData.email}</p>
+        <p>{formData.address}</p>
+        <p>{formData.mode}</p>
+        <p>{formData.addOns.map((add) => add)}</p>
+        <p>{formData.department}</p>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
